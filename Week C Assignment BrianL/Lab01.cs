5d9097b4c6c986f6180cfa6c1064777f7dc4db9b
blopezx <55446933+blopezx@users.noreply.github.com>
2020-04-24 17:30:58 -0500

    Add files via upload

---
+++ Week C Assignment BrianL/Lab01.cs
@@ -0,0 +1,55 @@
+﻿//Brian Lopez IT 483 Section 2 Suboh
+using System;
+
+namespace Lab01
+{
+  class resturantGuide
+  {
+    static void Main(string[] args)
+    {
+        Console.WriteLine("CS 201 Restaurant Guide\n");
+        bool spicy, fancy;
+        Console.WriteLine("Do you like spicy food? (y / n) : ");
+        string userResponse = Console.ReadLine();
+        char userLetter = userResponse[0];
+        if(userLetter == Convert.ToChar("y") || userLetter == Convert.ToChar("Y"))
+        {
+            spicy = true;
+        }
+        else
+        {
+            spicy = false;
+        }
+        Console.WriteLine("Do you want to go to a fancy restaurant? (y / n) : ");
+        string userResponse2 = Console.ReadLine();
+        char userLetter2 = userResponse2[0];
+        fancy = (userLetter2 == Convert.ToChar("y") || userLetter2 == Convert.ToChar("Y"));
+        // These are equal because the first one sets the spicy to true or false based on the user input of y
+        // The second one initalizes on false and changes based on the users input which similar to the one 
+        //above but done differently
+        if(spicy)
+        {
+            if (fancy)
+            {
+                Console.WriteLine("I suggest you go to Thai Garden Palace."); 
+            }
+            else 
+            {
+                Console.WriteLine("I suggest you go to Alberto's Tacqueria.");
+            }
+        }
+        else
+        {
+           if(fancy)
+           {
+               Console.WriteLine("I suggest you go to Chez Paris.");
+           } 
+           else
+           {
+               Console.WriteLine("I suggest you go to Joe's Diner.");
+           }
+        }
+
+    }
+  }
+}
---
+++ Week C Assignment BrianL/Lab02.cs
@@ -0,0 +1,66 @@
+﻿//Brian Lopez IT483 Section 2 Suboh
+using System;
+
+namespace Lab02
+{
+    class cashRegister  
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What kind of glasses would you like:");
+            Console.Write("1 -> Prescription, 2 -> Non-Prescription : ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            if(userInput == 1 || userInput == 2)
+            {
+                Console.WriteLine(""); 
+            }  
+            while(userInput != 1 && userInput != 2)
+            {
+                Console.Write("1 -> Prescription, 2 -> Non-Prescription : ");
+                userInput = Convert.ToInt32(Console.ReadLine());
+                if(userInput == 1 || userInput == 2)
+                {
+                    Console.WriteLine(""); 
+                }                         
+            }
+            
+            Console.WriteLine("What kind of coating would you like:");
+            Console.Write("1 -> Anti-glare, 2 -> Brown tint : ");
+            int userInput2 = Convert.ToInt32(Console.ReadLine());
+            if(userInput2 == 1 || userInput2 == 2)
+            {
+                Console.WriteLine(""); 
+            }          
+            while(userInput2 != 1 && userInput2 != 2)
+            {
+                Console.Write("1 -> Anti-glare, 2 -> Brown tint : ");
+                userInput2 = Convert.ToInt32(Console.ReadLine());
+                if(userInput2 == 1 || userInput2 == 2)
+                {
+                    Console.WriteLine(""); 
+                }             
+            }
+
+            double prescriptionPrice = 0, coatingPrice = 0;
+
+            if (userInput == 1)
+            {
+               prescriptionPrice = 40;
+            }
+            else if (userInput == 2)
+            {
+                prescriptionPrice = 25;
+            }
+            if (userInput2 == 1)
+            {
+                coatingPrice = 12.50;
+            }
+            else if (userInput2 == 2)
+            {
+                coatingPrice = 9.99;
+            }
+
+            Console.WriteLine("Your total cost is $" + (prescriptionPrice + coatingPrice));
+        }
+    }
+}
